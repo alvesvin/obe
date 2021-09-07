30a4798b5dad4f701a100b59beab4666371c889a
Cristhian Selbach <crsthian.04@gmail.com>
2021-09-07 17:59:12 -0300

    navbar

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,17 @@
 <script>
-	export let name;
+    import Navbar from "./components/Navbar.svelte";
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Navbar/>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap');
+*{
+    font-family: 'Poppins';
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
 </style>
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,99 @@
+<main>
+    <nav class="navbar mt-2">
+        <a href="/" class="navbar-brand">
+            <img src="./assets/brand.png" draggable="false" alt="" width="70px">
+        </a>
+        <div class="nav-items">
+            <a href="#home" class="item text-decoration-none mx-4">Home</a>
+            <a href="#details" class="item text-decoration-none mx-4">Détails</a>
+            <a href="#histoire" class="item text-decoration-none mx-4">Histoire</a>
+            <a href="#merch" class="item text-decoration-none mx-4">Merch</a>
+            <a href="#sommes-nous" class="item text-decoration-none mx-4">Où Sommes-Nous</a>
+        </div>
+    </nav>
+    <hr class="mt-2">
+</main>
+
+<style scoped>
+*{
+    margin: 0;
+    padding: 0;
+}
+
+main{
+    margin: 0px 80px 0px 80px;
+}
+
+a{
+    position: relative;
+    color: #2d3436!important;
+    font-weight: 600;
+    font-size: 19px;
+    display: inline-block;
+    transition: 0.2s ease-in;
+}
+
+a:hover{
+    -webkit-transform: scale(1.1);
+    color: rgb(0, 0, 0) !important;
+    cursor: pointer;
+    transform: scale(1.1);
+    transition: 0.2s ease-out;
+}
+
+.item::after{
+    content: "";
+    width: 100%;
+    height: 0.175rem;
+    position: absolute;
+    background-color: #ff7675;
+    left: 0;
+    bottom: -10px;
+    transform: scale(0, 1);
+    transition: 0.3s ease-in;
+    
+}
+.item:hover::after{
+    transform: scale(1, 1);
+    transition: 0.3s ease-out;
+}
+
+@media screen and (max-width: 1000px) {
+    .navbar-brand{
+        display: none;
+    }
+    .nav-items{
+        height: 80px;
+    }
+    .navbar{
+        align-items: center;
+        justify-content: center;
+    }
+    a{
+        margin-top: 30px;
+        font-size: 18px
+    }
+}
+
+@media screen and (max-width: 850px) {
+    a{
+        font-size: 17px;
+        margin-left: 7px !important;
+        margin-right: 7px !important;
+    }
+    
+}
+
+@media screen and (max-width: 660px) {
+    .navbar-brand{
+        display:block;
+    }
+    .navbar{
+        align-items: center;
+        justify-content: center;
+    }
+    .nav-items{
+        display: none;
+    }
+}
+</style>
